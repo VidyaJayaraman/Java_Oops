9099c248fad6b34053fe10dd23a8944cd61d775d
VidyaJayaraman <112783853+VidyaJayaraman@users.noreply.github.com>
2022-09-19 13:35:07 +0200

    Add files via upload

---
+++ College.java
@@ -0,0 +1,28 @@
+package org.college;
+
+
+// Super Class
+
+public class College {
+
+	
+	public void collegeName()
+	{
+		
+		System.out.println("College Name is Anna University");
+	}
+	
+	public void collegeCode()
+	{
+		
+		System.out.println("College code is 110");
+	}
+	public void collegeRank()
+	{
+		
+		System.out.println("College Rank is 1");
+	}
+	
+	
+
+}
