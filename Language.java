9c6f6b7087bf73c28065b751e8a0223dce6c5fbf
VidyaJayaraman <112783853+VidyaJayaraman@users.noreply.github.com>
2022-09-19 13:31:35 +0200

    Add files via upload

---
+++ Language.java
@@ -0,0 +1,9 @@
+package interface_abstract;
+
+// Interface
+
+public interface Language {
+	
+	public void java();
+
+}
---
+++ MultipleLanguage.java
@@ -0,0 +1,16 @@
+package interface_abstract;
+
+// Abstract Class
+
+public abstract class  MultipleLanguage  {
+	
+
+	public void python() {
+		
+		System.out.println("Language preference second is Python ");
+		
+	}
+	
+	public abstract void ruby();
+
+}
