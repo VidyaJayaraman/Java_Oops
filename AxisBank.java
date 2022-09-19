2f00c5c946b8b6683773f561e6a959d3d90c2786
VidyaJayaraman <112783853+VidyaJayaraman@users.noreply.github.com>
2022-09-19 13:32:41 +0200

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,21 @@
+package bankRecords;
+
+public class AxisBank extends BankInfo {
+	
+	// Override Method
+	
+	public void deposits() {
+		
+		System.out.println("Deposits interests upgraded to 15%");
+		
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
